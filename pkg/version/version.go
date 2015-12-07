d0fb1cce6d892e439f5c7e8e3fee4be33e60b801
Fabiano Franz <ffranz@redhat.com>
2015-12-07 20:49:10 -0200

    Basic command structure, trying to use tuf to diff versions

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	etcdversion "github.com/coreos/etcd/version"
+	updater "github.com/flynn/go-tuf/client"
+	"github.com/flynn/go-tuf/data"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/cobra"
 	kubeversion "k8s.io/kubernetes/pkg/version"
@@ -54,17 +56,68 @@ func (info Info) String() string {
 
 // NewVersionCommand creates a command for displaying the version of this binary
 func NewVersionCommand(basename string, printEtcdVersion bool) *cobra.Command {
-	return &cobra.Command{
+	baseUpdaterURL := "http://ocupdater-ffranz.rhcloud.com/updates/repository"
+	rootKeys := []*data.Key{
+		&data.Key{
+			"ed25519",
+			data.KeyValue{
+				[]byte("7b7e9101b855186da8c6072df197162c1f0fa641bcf29dc638a5e327002287ae"),
+				[]byte{},
+			},
+		},
+		&data.Key{
+			"ed25519",
+			data.KeyValue{
+				[]byte("cf488bab850635f2e9993c3ed4668b4b433ffbb8f1aaf750f97d7a27c2a0c1a9"),
+				[]byte{},
+			},
+		},
+	}
+
+	check := false
+	update := false
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Display version",
 		Run: func(c *cobra.Command, args []string) {
-			fmt.Printf("%s %v\n", basename, Get())
-			fmt.Printf("kubernetes %v\n", kubeversion.Get())
-			if printEtcdVersion {
-				fmt.Printf("etcd %v\n", etcdversion.Version)
+			switch {
+			case check:
+				localStore := updater.MemoryLocalStore()
+				remoteStore, err := updater.HTTPRemoteStore(baseUpdaterURL, &updater.HTTPRemoteOptions{})
+				if err != nil {
+					fmt.Println("Error fetching remote store: " + err.Error())
+					return
+				}
+				client := updater.NewClient(localStore, remoteStore)
+				if err := client.Init(rootKeys, len(rootKeys)); err != nil {
+					fmt.Println("Error initializing client: " + err.Error())
+					return
+				}
+				files, err := client.Targets()
+				if err != nil {
+					fmt.Println("Error listing targets: " + err.Error())
+					return
+				}
+				for _, file := range files {
+					fmt.Println(file.HashAlgorithms())
+				}
+				return
+
+			case update:
+				return
+
+			default:
+				fmt.Printf("%s %v\n", basename, Get())
+				fmt.Printf("kubernetes %v\n", kubeversion.Get())
+				if printEtcdVersion {
+					fmt.Printf("etcd %v\n", etcdversion.Version)
+				}
 			}
 		},
 	}
+	cmd.Flags().BoolVar(&check, "check", check, "Check if there is a new version of this client available")
+	cmd.Flags().BoolVar(&update, "update", update, "Auto-update this client if there is a new version available")
+	return cmd
 }
 
 func init() {
